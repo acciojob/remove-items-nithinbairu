fab7fec8d643de125f2c932a314008645767dde0
nithinbairu <113778598+nithinbairu@users.noreply.github.com>
2025-10-04 18:44:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+
+document.getElementById("button").addEventListener("click",function() {
+	let selectedColor=document.getElementById("colorSelect");
+	selectedColor.remove(selectedColor.selectedIndex);
+})
